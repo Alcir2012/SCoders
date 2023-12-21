70e161bfa64abbb7aeafc4f8b43b7c9e3dc55619
Alcir <josealcir.17@gmail.com>
2023-12-21 11:34:23 -0300

    changes

--- Aula3POO.py
+++ Aula3POO.py
@@ -1,8 +1,20 @@
 class Frase():
     def __init__(self,frase:str):
         self.Frase = frase
-        
+
+    def Conta_Caracteres(self):
+        caracteres = len(self.Frase)
+        return caracteres
+    
+    def Conta_Palavras(self):
+        palavras = len(self.Frase.split())
+        return palavras
+    def Inverte_Frase(self):
+        inverter = self.Frase[::-1]
+        return inverter
 fras = Frase('Bom dia princesa!')
+
 print(fras.Frase)
-quantidade_caracteres = len()
-print(f'Essa frase tem ',quantidade_caracteres,' caracteres')
+print(fras.Conta_Caracteres())
+print(fras.Conta_Palavras())
+print(fras.Inverte_Frase())
